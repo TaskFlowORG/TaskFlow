cdf1a2863bc407adf1dcebed67fe8e0a8769a868
Matheus Becker <beckerm@weg.net>
2023-11-10 21:19:07 -0300

    Criação do Footer, dark e light

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
 import Image from 'next/image'
 import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
 export default function Home() {
   return (
    <div className='h-screen w-screen'>
-      <Footer></Footer>
-      </div>
+      <Header></Header>
+   </div>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,36 @@ import Image from "next/image"
 
 export default function Header() {
 
-    return(
-            <div className="h-[55px] w-full shadow-md px-8 flex  items-center justify-between border bottom-2">
-               <img src="/Assets/iconLight.svg" alt="" height={'50px'} width={'50px'}/>
+    const theme = "light";
+    return (
+        <>
+            {theme == "light" &&
+                <div className="h-[55px] w-full bg-white shadow-md px-8 flex  items-center justify-between border bottom-2">
+                    <img src="/Assets/themeLight/iconLight.svg" alt="" height={'50px'} width={'50px'} />
 
-               <div className=" w-1/4 h-full flex space-x-[48px]  items-center justify-end">
-                <img src="/Assets/notification.svg" alt="" width={"20px"} hight={"20px"}/>
-                <img src="/Assets/Language.svg" alt="" width={"20px"} height={"20px"} />
-                <img src="/Assets/themeLight.svg" alt="" width={"20px"} height={"20px"} />
-                <img src="/Assets/Profile.svg" alt="" width={"20px"} height={"20px"}/>
-               </div>
+                    <div className=" w-1/4 h-full flex space-x-[48px]  items-center justify-end">
+                        <img src="/Assets/themeLight/notification.svg" alt="" width={"20px"} hight={"20px"} />
+                        <img src="/Assets/Language.svg" alt="" width={"20px"} height={"20px"} />
+                        <img src="/Assets/themeLight/themeLight.svg" alt="" width={"20px"} height={"20px"} />
+                        <img src="/Assets/themeLight/Profile.svg" alt="" width={"20px"} height={"20px"} />
+                    </div>
 
-            </div>
+                </div>
+            }
+            {theme == "dark" &&  <div className="h-[55px] w-full bg-back-grey shadow-md px-8 flex  items-center justify-between border bottom-2">
+                    <img src="/Assets/themeDark/iconDark.svg" alt="" height={'50px'} width={'50px'} />
 
+                    <div className=" w-1/4 h-full flex space-x-[48px]  items-center justify-end">
+                        <img src="/Assets/themeDark/notificationDark.svg" alt="" width={"20px"} hight={"20px"} />
+                        <img src="/Assets/Language.svg" alt="" width={"20px"} height={"20px"} />
+                        <img src="/Assets/themeDark/themeDark.svg" alt="" width={"20px"} height={"20px"} />
+                        <img src="/Assets/themeDark/ProfileDark.svg" alt="" width={"20px"} height={"20px"} />
+                    </div>
+
+                </div>
+}
+        </>
 
     )
-    
+
 }
