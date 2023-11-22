1aae2a89eb26d100c7b95227da393a55992e70e8
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-11-22 18:10:23 -0300

    Tentando ajeitar a animação

--- src/app/(after-login)/[user]/projects/page.jsx
+++ src/app/(after-login)/[user]/projects/page.jsx
@@ -12,101 +12,101 @@ export default function InitialPage({ color }) {
             percent: 75
         }, {
             id: 2,
-            name: "Projeto A",
+            name: "Projeto B",
             description: "Descrição do Projeto A",
             groups: [{ id: 1, image: "/next.svg" }, { id: 2, image: "/next.svg" }, { id: 3, image: "/next.svg" }],
             percent: 75
         }, {
             id: 3,
-            name: "Projeto A",
+            name: "Projeto C",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
         }, {
             id: 4,
-            name: "Projeto A",
+            name: "Projeto D",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         }, {
             id: 5,
-            name: "Projeto A",
+            name: "Projeto E",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         }, {
             id: 6,
-            name: "Projeto A",
+            name: "Projeto F",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 7,
-            name: "Projeto A",
+            name: "Projeto G",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 8,
-            name: "Projeto A",
+            name: "Projeto H",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
         }, {
             id: 5,
-            name: "Projeto A",
+            name: "Projeto I",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         }, {
             id: 6,
-            name: "Projeto A",
+            name: "Projeto J",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 7,
-            name: "Projeto A",
+            name: "Projeto K",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 8,
-            name: "Projeto A",
+            name: "Projeto L",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
         }, {
             id: 5,
-            name: "Projeto A",
+            name: "Projeto M",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         }, {
             id: 6,
-            name: "Projeto A",
+            name: "Projeto N",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 7,
-            name: "Projeto A",
+            name: "Projeto O",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
 
         },  {
             id: 8,
-            name: "Projeto A",
+            name: "Projeto P",
             description: "Descrição do Projeto A",
             groups: [],
             percent: 75
@@ -120,7 +120,7 @@ export default function InitialPage({ color }) {
                     <h1 className="h1 w-[90%] px-6 text-primary ">Projetos</h1>
                 </div>
                 <div className="w-4/5 h-[60vh] flex justify-center overflow-y-scroll">
-                    <div className=" w-[90%] justify-start flex flex-wrap p-6 gap-y-5 gap-x-12">
+                    <div className="justify-start grid p-6 gap-y-5 gap-x-12">
                         {projects.map(p => {
                             return <Project project={p} key={p.id} />
                         })}
--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProgressBar } from "../ProgressBar";
 import { Groups } from "../Groups";
 
@@ -8,23 +8,23 @@ export const Project = ({ project }) => {
 
   const [isHovering, setIsHovering] = useState(false);
 
-  return (
-    <div className={"w-80 flex flex-col shadow-blur-10 gap-16 bg-white duration-700 p-6 rounded-md overflow-clip  " + (isHovering ? " h-80" : "h-24")}
-      onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
-      <div className="flex gap-2 w-full">
-      {/* Imagem do Projeto */}
-        <div className="bg-zinc-200 rounded-md w-12 h-12"></div>
-        <div>
-          <h4 className="font-alata text-[16px] text-modal-grey">{project.name}</h4>
-          <p className="font-montserrat text-[12px] text-modal-grey">{project.description}</p>
+    return (
+      <div className={"w-80 flex flex-col shadow-blur-10 gap-16 bg-white duration-700 p-6 rounded-md overflow-clip  " + (isHovering ? " h-80  row-span-3" : "h-24  row-span-1")}
+        onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)}>
+        <div className="flex gap-2 w-full">
+          {/* Imagem do Projeto */}
+          <div className="bg-zinc-200 rounded-md w-12 h-12"></div>
+          <div>
+            <h4 className="font-alata text-[16px] text-modal-grey">{project.name}</h4>
+            <p className="font-montserrat text-[12px] text-modal-grey">{project.description}</p>
+          </div>
         </div>
+        {isHovering &&
+          <div className=" h-44 w-full justify-center flex flex-col gap-10">
+            <Groups groups={project.groups} max={4}></Groups>
+            <ProgressBar percent={project.percent} />
+          </div>
+        }
       </div>
-      {isHovering &&
-        <div className=" h-44 w-full justify-center flex flex-col gap-10">
-          <Groups groups={project.groups} max={4}></Groups>
-          <ProgressBar percent={project.percent} />
-        </div>
-      }
-    </div>
-  )
-}
+    )
+  }
