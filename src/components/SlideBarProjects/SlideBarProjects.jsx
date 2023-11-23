a0ce9e9d66731e2ce5f3160d43c56d3065bb8d16
Matheus Becker <beckerm@weg.net>
2023-11-22 21:52:53 -0300

    começo da implementação

---
+++ src/components/SlideBarProjects/SlideBarProjects.jsx
@@ -0,0 +1,55 @@
+export const SlideBarProjects = () => {
+
+
+    return (
+        <>
+            <div className="h-full w-1/5 flex justify-center items-center shadow-blur-10">
+                <div className="h-[90%] w-[90%] flex flex-col justify-center items-center">
+                    <div className="w-full h-1/4 flex items-center justify-around">
+                        <div className="h-1/3 w-1/5  bg-modal-grey rounded-md"></div>
+                        <div>
+                            <p className="h4 text-primary">Nome do projeto</p>
+                            <p className="p text-modal-grey">Início da descrição...</p>
+                        </div>
+                    </div>
+                    <div className="w-full h-1/4 flex flex-col items-center">
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary flex flex-row justify-center items-center">
+                            <img src="" alt="" className="h-full w-1/6 bg-primary" />
+                            <p className="p">Página Inicial</p>
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                    </div>
+                    <div className="w-full h-1/4 flex flex-col items-center">
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                        <div className="w-[80%] h-1/4  border-b-2 border-primary">
+
+                        </div>
+                    </div>
+                    <div className="w-full h-1/4">
+
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+
+    )
+
+}
