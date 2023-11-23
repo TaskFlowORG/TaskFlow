f40a6ad74d193b56863d5679990c892a83827e95
Matheus Becker <beckerm@weg.net>
2023-11-23 15:29:29 -0300

    Estilização finalida do componente

--- src/app/(before-login)/page.jsx
+++ src/app/(before-login)/page.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   return (
    <>
-    <div className="h-[85%] w-full">
+    <div className="h-full w-full">
       
         <SlideBarProjects></SlideBarProjects>
     </div>
