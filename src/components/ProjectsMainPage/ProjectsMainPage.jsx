a224a237153adec9fd0b4632e35acd867fa962d4
Jonatas Jackson Goncalves <jonatasg@weg.net>
2023-11-23 17:03:42 -0300

    ajuste da responsividade

--- src/components/ProjectsMainPage/ProjectsMainPage.jsx
+++ src/components/ProjectsMainPage/ProjectsMainPage.jsx
@@ -68,7 +68,7 @@ export const ProjectsMainPage = () => {
   }, [])
 
   return (
-    <div className="w-2/3 lg:w-1/3 flex flex-col gap-5 h-full lg:h-[70vh]">
+    <div className="w-2/3 lg:w-1/3 flex flex-col gap-5 h-[70vh]">
       <div className="w-full flex justify-center h-min p-2">
         <Link href={"./projects"} className="w-full">
           <button className="bg-primary w-full h-24 shadow-blur-10 text-white h4 rounded-md">
