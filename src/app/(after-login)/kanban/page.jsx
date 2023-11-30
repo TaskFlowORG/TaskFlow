2ff90de00a7f3439e79b8ff911b6f59e0e9d82dd
gabrielluka <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-11-30 18:29:57 -0300

    não implementa

--- src/app/(after-login)/kanban/page.jsx
+++ src/app/(after-login)/kanban/page.jsx
@@ -12,25 +12,124 @@ export default function Kanban() {
     const [id, setId] = useState(0);
     const [options, setOptions] = useState([]);
 
+    const page = {
+        id: 1,
+        name: "Page 1",
+        type: "KANBAN",
+        project: {
+            id: 2,
+            name: "Project 1",
+            description: "First Project Test in API",
+            date: null,
+            picture: null,
+            groups: []
+        },
+        properties: [],
+        tasks: [
+            {
+                id: 1002,
+                name: "Limpar a casa",
+                multiProperties: [],
+                uniProperties: [
+                    {
+                        taskId: 1002,
+                        propertyId: 1,
+                        value: "To-do",
+
+                        property: {
+                            id: 1,
+                            name: "Stats",
+                            visible: true,
+                            obligatory: false,
+                            type: "SELECT",
+                            options: [
+                                {
+                                    id: 1,
+                                    name: "To-do",
+                                    color: "#FF7A00"
+                                },
+                                {
+                                    id: 2,
+                                    name: "Done",
+                                    color: "#F04A94"
+                                },
+                                {
+                                    id: 3,
+                                    name: "Doing",
+                                    color: "#F7624B"
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        taskId: 1002,
+                        propertyId: 2,
+                        value: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi temporibus hic unde! Eaque totam, qui voluptates ea corrupti delectus cum eligendi quo possimus atque. Reprehenderit doloribus iure quo reiciendis sunt",
+
+                        property: {
+                            id: 2,
+                            name: "Description",
+                            visible: true,
+                            obligatory: false,
+                            type: "TEXT"
+                        }
+                    }
+                ]
+
+            }
+        ],
+
+        userProperties: [],
+        propertyOrdering: {
+            id: 1,
+            name: "Stats",
+            visible: true,
+            obligatory: false,
+            type: "SELECT",
+            options: [
+                {
+                    id: 1,
+                    name: "To-do",
+                    color: "#FF7A00"
+                },
+                {
+                    id: 2,
+                    name: "Done",
+                    color: "#F04A94"
+                },
+                {
+                    id: 3,
+                    name: "Doing",
+                    color: "#F7624B"
+                }
+            ]
+        }
+    }
+
 
     useEffect(() => {
 
-
-        const getPage = async () => {
-            const page = await getData("page", 1)
-
-            const tasks = page.tasks.map((taskPage) => {
-                return taskPage.task
-            })
-
-            const propertyOrdering = page.propertyOrdering;
-
-            console.log(page, tasks, propertyOrdering)
-        }
+        setOptions(page.propertyOrdering.options)
+        setTasks(page.tasks)
+        setId(page.propertyOrdering.id)
 
 
+        // const getPage = async () => {
+        //     const page = await getData("page", 1)
+        //     const tasks = page.tasks.map((taskPage) => {
+        //         return taskPage.task
+        //     })
 
-        getPage()
+        //     const propertyOrdering = page.propertyOrdering;
+
+        //     console.log(page, tasks, propertyOrdering)
+        // }
+
+
+
+        // getPage()
+
+
 
 
         // const getList = async () => {
@@ -62,12 +161,11 @@ export default function Kanban() {
         // getList();
     }, [id]);
 
-
     return (
         <>
             <div className="w-full h-full mt-[5em] flex flex-col dark:bg-back-grey">
                 <div className="flex gap-5 items-end pb-16 justify-center    h-max">
-                    <h1 className="h1 text-primary whitespace-nowrap dark:text-white">Page Name</h1>
+                    <h1 className="h1 text-primary whitespace-nowrap dark:text-white">{page.name}</h1>
                     <div className=" flex items-center justify-center h-9 w-9 rounded-full shadowww mb-4 ">
                         <p className="p text-primary text-4xl h-min w-min">+</p>
                     </div>
@@ -80,7 +178,7 @@ export default function Kanban() {
                                 key={option.id}
                                 tasks={tasks}
                                 propertyId={id}
-                                color="#FF0000"
+                                color={option.color}
                                 id={option.id}
                                 option={option.name}
                                 columnName={"To Do"}
@@ -88,13 +186,13 @@ export default function Kanban() {
                             />
                         );
                     })}
+
                     {
                         <ColumnKanban
                             key={0}
                             tasks={defaultTasks}
                             propertyId={id}
                             color="#FF0000"
-
                             option={"Não marcadas"}
                             columnName={"To Do"} />
                     }
