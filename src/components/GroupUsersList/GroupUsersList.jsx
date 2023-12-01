ec8dff29a82f3fb6ef4f9e19713bce7d1f05633a
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2023-12-01 16:36:38 -0300

    pipipipopopo

--- src/components/GroupUsersList/GroupUsersList.jsx
+++ src/components/GroupUsersList/GroupUsersList.jsx
@@ -24,13 +24,13 @@ export const GroupUsersList = ({ userId }) => {
     return (
         <div>
             <div className="">
-                <div className="border rounded-md border-[#F04A94] px-4 pr-6 bg-[#FCFCFC]  dark:bg-[#3C3C3C] h-12 flex items-center justify-between">
+                <div className="border rounded-md border-[#F04A94] px-4 pr-6 bg-[#FCFCFC]  dark:bg-[#3C3C3C] dark:border-[#F76858] h-12 flex items-center justify-between">
                     <div className="flex gap-6">
                         <img className="" src="/img/User.svg" />
-                        <p className="whitespace-nowrap text-black ">{user.name}</p>
+                        <p className="whitespace-nowrap dark:text-[#FCFCFC] text-black ">{user.name}</p>
                     </div>
-                    <div className="text-[#F04A94] flex gap-7  ">
-                        <p className="">|</p>
+                    <div className="text-[#F04A94] dark:text-[#F76858] flex gap-7">
+                        <p>|</p>
                         <p onClick={handleClick}>Permission</p>
                         {showModal && <ModalPermission />}
                     </div>
