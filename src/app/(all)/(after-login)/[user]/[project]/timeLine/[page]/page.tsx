c5ce26ba28866265357632174b55596052d15ed5
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2024-03-04 21:56:48 -0300

    chore:resolve merge in timeline

--- src/app/(all)/(after-login)/[user]/[project]/timeLine/[page]/page.tsx
+++ src/app/(all)/(after-login)/[user]/[project]/timeLine/[page]/page.tsx
@@ -1,58 +1,70 @@
-"use client"
-import { RegisterProperty } from "@/components/RegisterProperty"
-import { Page, Project } from "@/models"
-import { pageService, projectService } from "@/services"
-import { useEffect, useState } from "react"
+"use client";
+import { RegisterProperty } from "@/components/RegisterProperty";
+import { Page, Project } from "@/models";
+import { pageService, projectService } from "@/services";
+import { useEffect, useState } from "react";
 
+export default function ({
+  params,
+}: {
+  params: { project: number; page: number };
+}) {
+  const [project, setProject] = useState<Project>();
+  const [page, setPage] = useState<Page>();
 
-export const TimeLine = ({ params }: { params: { project: number, page: number } }) => {
-    const [project, setProject] = useState<Project>()
-    const [page, setPage] = useState<Page>()
+  useEffect(() => {
+    (async () => {
+      const projectPromisse = await projectService.findOne(params.project);
+      setProject(projectPromisse);
+      const pagePromisse = await pageService.findOne(params.page);
+      setPage(pagePromisse);
+    })();
+  }, [params.project, params.page]);
+  interface Task {
+    date: Date;
+    id: number;
+    name: string;
+    uniProperties: Array<PropertyValue>;
+    userProperties: Array<PropertyValue>;
+    multiProperties: Array<MultiPropertyValue>;
+  }
+  interface Property {
+    id: number;
+    type: string;
+    visible: true;
+  }
+  interface PropertyValue {
+    property: Property;
+    value: string;
+  }
+  interface MultiPropertyValue {
+    property: Property;
+    value: Array<string>;
+  }
 
-    useEffect(() => {
-        (async () => {
-            const projectPromisse = await projectService.findOne(params.project)
-            setProject(projectPromisse)
-            const pagePromisse = await pageService.findOne(params.page)
-            setPage(pagePromisse)
-        })()
-    }, [params.project, params.page])
-    interface Task {
-        date: Date,
-        id: number,
-        name: string,
-        uniProperties: Array<PropertyValue>,
-        userProperties: Array<PropertyValue>,
-        multiProperties: Array<MultiPropertyValue>,
-
-    }
-    interface Property {
-        id: number,
-        type: string,
-        visible: true
-    }
-    interface PropertyValue {
-        property: Property,
-        value: string
-    }
-    interface MultiPropertyValue {
-        property: Property,
-        value: Array<string>
-    }
-
-
-    const [modalProperty, setModalProperty] = useState(false)
-    return (
-        <>
-   <div className="h-full w-full">
-                <div className=" flex items-center justify-center h-10 w-10  rounded-full  shadowww cursor-pointer bottom-10 right-10 fixed hover:bg-primary" onClick={()=> {setModalProperty(true)}}>
-                    <p className="h5 text-primary h-min w-min hover:text-white">+</p>
-                </div>
-                <RegisterProperty open={modalProperty} project={project!} page={page} properties={[...project?.properties ?? [], ...page?.properties ?? []]} close={() => setModalProperty(false)} />
-
-
-            </div>
-
-        </>
-    )
+  const [modalProperty, setModalProperty] = useState(false);
+  return (
+    <>
+      <div className="h-full w-full">
+        <div
+          className=" flex items-center justify-center h-10 w-10  rounded-full  shadowww cursor-pointer bottom-10 right-10 fixed hover:bg-primary"
+          onClick={() => {
+            setModalProperty(true);
+          }}
+        >
+          <p className="h5 text-primary h-min w-min hover:text-white">+</p>
+        </div>
+        <RegisterProperty
+          open={modalProperty}
+          project={project!}
+          page={page}
+          properties={[
+            ...(project?.properties ?? []),
+            ...(page?.properties ?? []),
+          ]}
+          close={() => setModalProperty(false)}
+        />
+      </div>
+    </>
+  );
 }
