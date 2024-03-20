e93041b12ec75cc9cb33d5f216a2629a946440b8
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-03-19 21:48:04 -0300

    Estou adicionando os inputs

--- src/app/(all)/(after-login)/[user]/[project]/group/page.tsx
+++ src/app/(all)/(after-login)/[user]/[project]/group/page.tsx
@@ -3,13 +3,10 @@
 import { Description } from "@/components/Description/Description";
 import { SVGGroupPageD } from "@/components/SVGGroupPageDark/SVGGroupPageD";
 import { SVGGroupPageL } from "@/components/SVGGroupPageLight/SVGGroupPageL";
-import { SVGGroupMobileL } from "@/components/SVGGroupMobileL/SVGGroupMobileL"
 import { UsersList} from "@/components/UsersList/UsersList";
 import { useEffect, useState } from "react"
 import { useTheme } from "next-themes";
-import { SVGGroupMobileD } from "@/components/SVGGroupMobileD/SVGGroupMobileD";
 import { getData} from "@/services/http/api";
-
 
 export default function Home({ projectId = 1 }) {
     const { theme, setTheme } = useTheme();
@@ -24,26 +21,24 @@ export default function Home({ projectId = 1 }) {
     }, []);
 
     const svgGroupPage = theme === "dark" ? <SVGGroupPageD /> : <SVGGroupPageL />;
-    const svgGroupMobile = theme === "dark" ? <SVGGroupMobileD/> : <SVGGroupMobileL />;
 
     return (
         <div className=" w-screen h-screen ">
             <div className="flex absolute md:-bottom-36 xl: 2xl:bottom-0 lg:z-10 ">
                 {svgGroupPage}
             </div>
-            {/* <div className="flex lg:hidden md:hidden pt-56 sm:pt-72  max-h-screen overflow-clip absolute">
-                {svgGroupMobile}
-            </div> */}
 
-            <div className="w-full  flex flex-col lg:flex-row lg:gap-32 mt-32">
+            <div className="w-full flex flex-col lg:flex-row lg:gap-32 mt-32">
+
               <div className="flex flex-col lg:flex-row w-1/2 lg:justify-end">
                 <Description project={project} />
             </div>
+
             <div className="flex flex-col lg:flex-row lg:w-1/2 mt-12 lg:mt-0">
                 <UsersList project={project} />
             </div>   
+
             </div>
-             
         </div>
     )
 }
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,13 +1,37 @@
-export const Register = ({}) =>{
+import { Form } from "react-hook-form"
+import { Input } from "@/components/Input";
 
-    return(
-       <div className="w-full h-full">
-         <div className="flex absolute md:-bottom-36 xl: 2xl:bottom-0 lg:z-10 ">
-                <img src="/img/Register.png"/>
-            </div>
-            <div className=" flex justify-end z-50"><img src="/img/MiddleRegister.png"/>
-            </div>
-        <div className="w-32 bg-[#FCFCFC] "></div>
-       </div>
-    )
+export const Register = ({ }) => {
+    
+
+    return (
+        <div className="flex h-5/6 w-screen absolute justify-center items-center">
+            <div className="flex items-center flex-col h-1/2 w-1/4 shadow-blur-10 rounded-md bg-white dark:bg-modal-grey  justify-between py-8">
+                <h4 className="h4">Registrar</h4>
+                <div>
+                    <form>
+                        // onSubmit={handleSubmit(handleRegister)}
+                        
+                    
+                        <p className="h4">Registrar</p>
+
+                        {value === 0 && (
+                            <>
+                                <Input
+                                    className="inputRegister"
+                                    image={"Assets/themelight/IconUser.svg"}
+                                    placeholder="Digite seu nome"
+                                    value={user.name}
+                                    helperText={"errors.name?.message"}
+                                    register={ "...register(name) "}
+                                    required
+                                    classNameInput={
+                                        "w-5/6 h-full outline-none  px-5 dark:bg-modal-grey"
+                                    }
+                                />
+                            </form>
+                            </div>
+                            </div>
+                    </div>
+            )
 }
