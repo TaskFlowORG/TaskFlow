8d4e6cf57bb06c5e170cf57c02c23bd733763e09
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-03-21 08:38:42 -0300

    configuring axios ip to update in vercel

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -1,8 +1,40 @@
-import axios from "axios";
+import axios from 'axios';
 
+
+const urls =[
+    'http://localhost:9999/',
+    'http://192.168.0.14:9999/',
+]
 
 export const Api = axios.create({
     // baseURL: 'http://10.4.96.33:9999/'
-    baseURL: 'http://localhost:9999/'
+    baseURL: urls[0]
 
 });
+
+Api.interceptors.response.use(
+    (    response: any) => response,
+    (error:any) => {
+      const { config } = error;
+      const originalRequest = config;
+  
+      // Se houver mais URLs na lista, tente a próxima
+      if (urls.indexOf(originalRequest.baseURL) !== urls.length - 1) {
+        originalRequest.baseURL = urls[urls.indexOf(originalRequest.baseURL) + 1];
+        return axios(originalRequest);
+      }
+  
+      return Promise.reject(error);
+    }
+  );
+  
+    Api.get('/endpoint')
+        .then((response:any) => {
+            console.log(response.data);
+        })
+        .catch((error:any) => {
+            console.error('Error:', error);
+        });
+
+
+
