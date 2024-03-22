33106318f4940e91844bb88043dc165271b66e8a
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-03-21 21:57:08 -0300

    falta o icon branco

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -25,6 +25,7 @@ export const Description: React.FC<Props> = ({ project, groupId = 1 }) => {
         getList();
     }, [groupId]);
 
+
     return (
         <div>
             {
--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,5 +1,5 @@
-'use client'
 
+'use client'
 import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/Input";
 import { userService } from "@/services";
 import { UserPost } from "@/models";
+import { useTheme } from "next-themes";
 
 const schema = z
   .object({
@@ -45,7 +46,9 @@ const schema = z
     }
   );
 
+
 type FormData = z.infer<typeof schema>;
+
 
 interface UserData {
   name: string;
@@ -56,12 +59,16 @@ interface UserData {
   confirmPassword: string;
 }
 
+
 export const Register = () => {
   const [step, setStep] = useState(0);
   const { register, handleSubmit, formState: { errors } } = useForm<UserData>({
     resolver: zodResolver(schema)
   });
   const [user, setUser] = useState({} as FormData);
+  const { theme, setTheme } = useTheme();
+
+
 
   const handleNextStep = () => {
     if (step < 2) {
@@ -69,11 +76,13 @@ export const Register = () => {
     }
   };
 
+
   const handlePrevStep = () => {
     if (step > 0) {
       setStep(step - 1);
     }
   };
+
 
   const onSubmit = async (data: UserData) => {
     try {
@@ -84,19 +93,22 @@ export const Register = () => {
         console.error(err.errors);
       }
     }
-  };
-  
+  }
+
+
+
+  const imageUser = theme === "light" ? "/img/themeLight/IconUser.svg" : "/img/themeDark/userIcon.svg";
 
   return (
-    <div className="flex h-5/6 w-screen absolute justify-center items-center">
-      <div className="flex items-center flex-col md:h-1/2 lg:w-1/4 md:w-1/2 w-10/12 shadow-blur-10 rounded-md bg-white dark:bg-modal-grey  justify-between py-8">
+    <div className="flex h-5/6 w-screen absolute justify-center items-center  text-[#333] dark:text-[#FCFCFC]">
+      <div className="flex items-center flex-col md:h-1/2 lg:w-2/6 md:w-1/2 w-10/12 1.5xl:w-1/4 shadow-blur-10 rounded-md bg-white dark:bg-modal-grey  justify-between py-8">
         <h4 className="h4 py-3 md:py-0">Registar</h4>
         <form onSubmit={handleSubmit(onSubmit)} className="h-4/5 w-4/5 flex flex-col items-center justify-between">
           {step === 0 && (
             <>
               <Input
                 className="inputRegister"
-                image={"/img/themelight/IconUser.svg"}
+                image={imageUser}
                 placeholder="Digite seu nome"
                 value={user.name}
                 helperText={errors.name?.message}
@@ -107,7 +119,7 @@ export const Register = () => {
               {console.log()}
               <Input
                 className="inputRegister"
-                image={"/img/themelight/IconUser.svg"}
+                image={imageUser}
                 placeholder="Digite seu sobrenome"
                 value={user.surname}
                 helperText={errors.surname?.message}
@@ -118,11 +130,12 @@ export const Register = () => {
             </>
           )}
 
+
           {step === 1 && (
             <>
               <Input
                 className="inputRegister"
-                image={"/img/themelight/IconUser.svg"}
+                image={imageUser}
                 placeholder="Digite seu nome de usuário"
                 helperText={errors.username?.message}
                 value={user.username}
@@ -142,6 +155,7 @@ export const Register = () => {
               />
             </>
           )}
+
 
           {step === 2 && (
             <>
@@ -167,6 +181,7 @@ export const Register = () => {
             </>
           )}
 
+
           <div className="flex justify-between w-full mt-4">
             {step > 0 && (
               <button type="button" onClick={handlePrevStep} className="font-alata text-sm underline text-[#282828] hover:cursor-pointer hover:text-[#F04A94]">
@@ -190,6 +205,11 @@ export const Register = () => {
         </form>
       </div>
 
+
     </div>
   );
 };
+
+
+
+
