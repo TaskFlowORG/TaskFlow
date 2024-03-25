5056ad45cf084e61310aa50fac6f5ee72dc21d3a
Jonatas Jackson Goncalves <jonatasg@weg.net>
2024-03-25 20:37:11 -0300

    agora vai, confia vercel

--- src/app/(all)/(after-login)/[user]/[project]/[page]/page.tsx
+++ src/app/(all)/(after-login)/[user]/[project]/[page]/page.tsx
@@ -1,11 +1,25 @@
+"use client"
+
 import { Calendar, Canvas, Kanban, List, Table, TimeLine } from "@/components/Pages";
-import { CanvasPage, OrderedPage,  TypeOfPage } from "@/models";
+import { CanvasPage, OrderedPage,  Page,  TypeOfPage, User } from "@/models";
 import { pageService, projectService, userService } from "@/services";
+import { ProjectContext } from "@/utils/ContextProject";
+import { useContext, useEffect, useState } from "react";
    
-export default async function Pages({params}:{params:{user:string, project:number, page:number}}){
-    const page = await pageService.findOne(params.page)
-    const project = await projectService.findOne(params.project)
-    const user = await userService.findByUsername(params.user)
+export default function Pages({params}:{params:{user:string, project:number, page:number}}){
+    const [page, setPage] = useState<Page>()
+    const {project} = useContext(ProjectContext)
+    const [user, setUser] = useState<User>()
+
+    useEffect(()=>{
+        (async () => {
+            const pageT = await pageService.findOne(params.page)
+            const userT = await userService.findByUsername(params.user)
+            setPage(pageT)
+            setUser(userT)
+        })()
+    },[params])
+
     switch(page?.type){
         case TypeOfPage.CALENDAR:
             return <Calendar page={page as OrderedPage} />
@@ -14,10 +28,10 @@ export default async function Pages({params}:{params:{user:string, project:numbe
         case TypeOfPage.LIST:
             return <List page={page} />
         case TypeOfPage.TABLE:
-            return <Table page={page} project={project} />
+            return <Table page={page} project={project!} />
         case TypeOfPage.TIMELINE:
             return <TimeLine />
         case TypeOfPage.CANVAS:
-            return <Canvas page={page as CanvasPage} user={user} />
+            return <Canvas page={page as CanvasPage} user={user!} />
     }
 }
