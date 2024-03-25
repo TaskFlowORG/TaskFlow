c1101094711d59210f49ba6f2b9c88cd737e8c85
Jonatas Jackson Goncalves <jonatasg@weg.net>
2024-03-25 20:30:37 -0300

    provavelmente ultima tentativa

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -3,11 +3,12 @@ import axios from 'axios';
 
 const urls =[
     'http://localhost:9999/',
+    "http://10.4.96.36:9999/"
 ]
 
 export const Api = axios.create({
     // baseURL: 'http://10.4.96.33:9999/'
-    baseURL: urls[0]
+    baseURL: urls[1]
 
 });
 
