6d505ded88a284eecd01aac40eac5ee5b5f30521
Jonatas Jackson Goncalves <jonatasg@weg.net>
2024-03-25 20:24:26 -0300

    mais uma  tentativa vercel

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -11,29 +11,5 @@ export const Api = axios.create({
 
 });
 
-Api.interceptors.response.use(
-    (    response: any) => response,
-    (error:any) => {
-      const { config } = error;
-      const originalRequest = config;
-  
-      // Se houver mais URLs na lista, tente a próxima
-      if (urls.indexOf(originalRequest.baseURL) !== urls.length - 1) {
-        originalRequest.baseURL = urls[urls.indexOf(originalRequest.baseURL) + 1];
-        return axios(originalRequest);
-      }
-  
-      return Promise.reject(error);
-    }
-  );
-  
-    Api.get('/endpoint')
-        .then((response:any) => {
-            console.log(response.data);
-        })
-        .catch((error:any) => {
-            console.error('Error:', error);
-        });
-
 
 
