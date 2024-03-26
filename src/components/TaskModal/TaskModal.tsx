b6ad69b42f9af368392bbe5992a685c6e80c1057
Gabriel Luka de Amaral <gabrielluka@weg.net>
2024-03-26 19:03:24 -0300

    comments-completeds

--- src/components/TaskModal/TaskModal.tsx
+++ src/components/TaskModal/TaskModal.tsx
@@ -17,7 +17,7 @@ type isOpenBro = {
 };
 
 export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState("");
   // const [url, setUrl] = useState("");
   // async function findImage() {
   //   let bah = await (await axios.get("http://localhost:9999/aws/1")).data;
@@ -25,21 +25,21 @@ export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
   // }
 
   async function sendComment() {
-    let comment:Partial<MessageGet> = {
-      sender:user,
-      value:input,
-    }
-    // task.task.comments.push(comment)
-
+    let comment: MessageGet = {
+      sender: user,
+      value: input,
+    };
+    task.task.comments.push(comment);
     await taskService.upDate(task.task);
+    setInput("");
   }
   return (
     <CenterModal
-      stylesTailwind={"w-[800px] shadow-blur-10 p-12"}
+      stylesTailwind={"w-[1306px] shadow-blur-10 p-12"}
       condition={isOpen}
       setCondition={setIsOpen}
     >
-      <div className="flex justify-between w-full h-full">
+      <div className="flex gap-[102px]  w-full h-full">
         <div className="flex flex-col gap-12 w-[453px]">
           <h3 className="h3 whitespace-nowrap">
             {task?.task.name ?? "Sem nome"}
@@ -56,23 +56,26 @@ export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
               </button>
             </div>
             <div className=" flex flex-col gap-6">
-              {task?.task.comments.map((comment, index) => {
-                return (
-                  <Comment
-                    user={user}
-                    sender={comment.sender}
-                    value={comment.value}
-                    date={comment.dateCreate.toString()}
-                    key={index}
-                  ></Comment>
-                );
-              })}
+              <div className="flex flex-col gap-6 max-h-[442px] overflow-auto pr-8 bah">
+                {task?.task.comments.map((comment, index) => {
+                  return (
+                    <Comment
+                      user={user}
+                      sender={comment.sender}
+                      value={comment.value}
+                      date={comment.dateCreate!.toString()}
+                      key={index}
+                    ></Comment>
+                  );
+                })}
+              </div>
               <div className="flex gap-6">
                 <input
                   type="text"
+                  value={input}
                   className="text-[14px] border-[#d7d7d7] border-[1px] shadow-comment bg-[#f2f2f2] flex-1 font-montserrat px-3 py-[10px] rounded-lg"
                   placeholder="Escreva o comentário"
-                  onChange={(e)=> setInput(e.target.value)}
+                  onChange={(e) => setInput(e.target.value)}
                 />
                 <div
                   className="h-full items-center flex justify-center aspect-square rounded-lg bg-primary"
@@ -82,6 +85,32 @@ export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+        <div className=" w-[2px] min-h-full bg-[#F2F2F2]"></div>
+        <div className="w-full max-w-[547px]">
+          <div className="flex flex-col gap-5 max-h-[450px] overflow-auto bah bg-black w-full">
+            {task?.task.properties.map((prop) => {
+              return (
+                <div key={prop.id} className="bg-white flex flex-col">
+                  {JSON.stringify(prop.property.name)}
+                  <div className="flex gap-8 w-full">
+                    <span>C</span>
+                    <div className="flex-1 flex justify-between bg-purple-400">
+                      <div className="flex gap-8">
+                        <span>I</span>
+                        <p className="font-montserrat text-[16px]">
+                          {prop.property.name}
+                        </p>
+                      </div>
+                      <div className="bg-red-600 flex-1 flex justify-end">
+                        a
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
