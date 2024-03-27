8f6a893c0746f707ff9fcd58b6bf12fa4f072538
Gabriel Fellipe Marquardt <gfellipe@weg.net>
2024-03-26 21:55:22 -0300

    Refazendo a pagina de configuracoes gerais

--- src/models/user/user/SimpleUserGetDTO.ts
+++ src/models/user/user/SimpleUserGetDTO.ts
@@ -9,7 +9,7 @@ export class    SimpleUserGet {
     name!: string;
     surname!: string;
     address?: string;
-    //picture?: Archive;
+    picture?: Archive;
     mail!: string;
     phone?: string;
     description?: string;
