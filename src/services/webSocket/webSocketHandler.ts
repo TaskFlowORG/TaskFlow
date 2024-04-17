bf84036999472c4543c4e1806f84cfe92eb07aa5
Matheus Becker <beckerm@weg.net>
2024-04-17 19:45:09 -0300

    feat : WebSocket concluded

--- src/services/webSocket/webSocketHandler.ts
+++ src/services/webSocket/webSocketHandler.ts
@@ -1,17 +1,16 @@
 import { Stomp, Client, IMessage } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
 
-
-
-
-
-export const onConnect = (topic: string, handle:(message:IMessage) => void) => {
-    const socket = new SockJS('http://localhost:9999/notifications');
-const client = Stomp.over(socket);
-    client.connect({}, () => {
-        console.log("connected");
-        client.subscribe(topic, (message) => {
-            handle(message);
-        });
-    }); 
-}
+export const onConnect = (
+  topic: string,
+  handle: (message: IMessage) => void
+) => {
+  const socket = new SockJS("http://localhost:9999/notifications");
+  const client = Stomp.over(socket);
+  client.connect({}, () => {
+    console.log("connected");
+    client.subscribe(topic, (message) => {
+      handle(message);
+    });
+  });
+};
