4509258c7c14d99717955d74bcd9d28e44ac952a
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-04-24 21:52:50 -0300

    tentativa de transition

--- src/animations.ts
+++ src/animations.ts
@@ -1,6 +1,7 @@
 import gsap from "gsap";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { ModalRegisterProperty } from "./components/ModalRegisterProperty";
+import { CustomEase } from "gsap/CustomEase";
 
 export const animatePageOut = (href: string, router: AppRouterInstance) => {
     const transitionElementModalRegister = document.getElementById("modalRegister");
@@ -45,62 +46,66 @@ export const animatePageOut = (href: string, router: AppRouterInstance) => {
             ease: "power1.inOut"
         });
     }
-    const x = (window.innerWidth + 300) / 5;
+    const x = (window.innerWidth + 300);
 
     // Animations for the register
     const animationWrapperRegister = document.getElementById("register")
     if (animationWrapperRegister) {
         const tlRegister = gsap.timeline();
-        tlRegister
-            .to(animationWrapperRegister, {
-                delay: 0.5,
-                yPercent: 17,
-                x: x * 0.5,
-                duration: 1,
-                ease: "power1.in"
-            })
-            .to(animationWrapperRegister, {
-                yPercent: 34,
-                x: x * 1.25,
-                duration: 0.5,
-                ease: "none"
-            }).to(animationWrapperRegister, {
-                yPercent: 50,
-                x: x * 2,
-                duration: 0.5,
-                ease: "none"
-            }).to(animationWrapperRegister, {
-                yPercent: 52,
-                x: x * 2.5,
-                duration: 0.3,
-                ease: "none"
-            }).to(animationWrapperRegister, {
-                yPercent: 50,
-                x: x * 3,
-                duration: 0.3,
-                ease: "none"
-            }).to(animationWrapperRegister, {
-                yPercent: 34,
-                x: x * 3.75,
-                duration: 0.5,
-                ease: "none"
-            }).to(animationWrapperRegister, {
-                yPercent: 17,
-                x: x * 4.5,
-                duration: 0.5,
-                ease: "none"
-            })
-            .to(animationWrapperRegister, {
-                yPercent: 0,
-                x: x * 5,
-                duration: 1,
-                ease: "power2.out",
+        for (let i = 1; i <= 100; i++) {
+            const break1 = i <12.5;
+            const break2 = i < 25;
+            const break3 = i < 37.5;
+            const break4 = i < 50;
+
+            const break9 = i < 62.5;
+            const break6 = i < 75;
+            const break7 = i < 87.5;
+            const break8 = i < 100;
+
+
+
+
+            const equal0 = i == 1;
+            const equal99 = i == 100;
+    
+            // Função de easing personalizada
+            const customEase = CustomEase.create("custom", "M0,0 C0.1,0.2,0.3,1,1,1");
+    
+            tlRegister.to(animationWrapperRegister, {
+                delay: equal0 ? 0.5 : 0,
+                yPercent: i < 50 ? i : 100 -i,
+                x: x / 100 * (break1 ? i/4 : break2 ? i/3 : break3 ? i/2 : break4 ? i : break9 ? 50 + i : break6 ? 50 + i/2 : break7 ? 50 + i/3: 50 + i/4),
+                duration: 0.01,
+                ease: equal0 ? "power1.in" : equal99 ? "power2.out" : customEase,
                 onComplete: () => {
-                    router.push(href);
+                    if (i == 99) {
+                        router.push(href);
+                    }
                 }
             });
-
+        }
     }
+
+    function calcularConstante() {
+        let somaTotal = 0;
+
+        // Calcula a soma total de 1/i para i de 1 a 50
+        for (let i = 1; i <= 50; i++) {
+            somaTotal += 1 / i;
+        }
+
+        // Calcula a constante necessária para garantir que a soma seja 50
+        const constante = 50 / somaTotal;
+
+        return constante;
+    }
+
+    // Função para calcular a angulação com a constante
+    function calcularAngulacao(constante: number, i: number) {
+        return constante * (1 / i);
+    }
+
 
     // Animations for the login
     const animationWrapperLogin = document.getElementById("login");
@@ -184,7 +189,7 @@ export const animatePageIn = () => {
             duration: 0.5,
             ease: "power1.inOut"
         });
-    }else if(transitionElementModalLogin){
+    } else if (transitionElementModalLogin) {
         const tlModal = gsap.timeline({
             stagger: {
                 _amount: 0, // No pause between animations within the timeline
@@ -204,7 +209,7 @@ export const animatePageIn = () => {
             duration: 0.5,
             ease: "power1.inOut"
         });
-        
+
     }
 
 }
