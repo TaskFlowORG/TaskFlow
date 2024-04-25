590473db6f36dfd222102a1ca072e03a9e1b872f
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-04-25 14:09:22 -0300

    chore: group

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -3,6 +3,7 @@ import { getListData, getData } from "@/services/http/api";
 import { GroupAccess } from "../GroupAccess/GroupAccess";
 import { Group, Project } from "@/models";
 import { groupService } from "@/services";
+import { SimpleGroup } from "@/models/user/group/SimpleGroup";
 
 interface Props {
     user: string;
@@ -11,7 +12,7 @@ interface Props {
 }
 
 export const Description: React.FC<Props> = ({ user, project, groupId }) => {
-    const [groups, setGroups] = useState<Group[]>([]);
+    const [groups, setGroups] = useState<SimpleGroup[]>([]);
 
     useEffect(() => {
         const getList = async () => {
@@ -31,7 +32,7 @@ export const Description: React.FC<Props> = ({ user, project, groupId }) => {
             {
                 groups.map((g) => {
                     if (g.id === Number(groupId)) {
-                        return <GroupAccess key={g.id} project={project} group={g} />
+                        return <GroupAccess key={g.id} project={project} groupId={g} />
                     }
                     return null;
                 })
--- src/services/services/GroupService.ts
+++ src/services/services/GroupService.ts
@@ -25,7 +25,7 @@ class GroupService {
     }
 
     async findGroupsByUser(): Promise<SimpleGroup[]> {
-        const response = await Api.get<Group[]>("group/my", { withCredentials: true });
+        const response = await Api.get<Group[]>("group", { withCredentials: true });
         return response.data;
     }
     async findAll(): Promise<SimpleGroup[]> {
@@ -33,7 +33,7 @@ class GroupService {
         return response.data;
     }
 
-    async findGroupsByAProject(projectId: number): Promise<SimpleGroup[]> {
+    async findGroupsByAProject(projectId: number): Promise<Group[]> {
         const response = await Api.get<Group[]>(`group/project/${projectId}`, { withCredentials: true });
         return response.data;
     }
