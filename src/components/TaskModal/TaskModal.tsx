183c475549d53087039488424eccc2bb96e58805
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2024-04-28 11:43:31 -0300

    style: continue componentization of modalTask, and organize functions better

--- src/components/TaskModal/TaskModal.tsx
+++ src/components/TaskModal/TaskModal.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { CenterModal } from "../Modal";
 import { Task, User, OtherUser } from "@/models";
 
-import { taskService, userService } from "@/services";
+import { userService } from "@/services";
 import { ProjectContext } from "@/contexts/ContextProject";
-import { FilterContext } from "@/utils/FilterlistContext";
-import { FilteredProperty } from "@/types/FilteredProperty";
-import { useTranslation } from "next-i18next";
-import { PageContext } from "@/utils/pageContext";
-import { PropertiesSide } from "./PropertiesSide";
-import { CommentsSection } from "./CommentsAndHistoric/CommentsSection";
-import { HeaderCommentAndHistoric } from "./CommentsAndHistoric/HeaderCommentAndHistoric";
+
+import { TaskModalContent } from "./TaskModalContent";
 
 type isOpenBro = {
   isOpen: boolean;
@@ -22,63 +17,6 @@ type isOpenBro = {
 };
 
 export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
-  const [filter, setFilter] = useState<FilteredProperty[]>([]);
-  const [list, setList] = useState<FilteredProperty | null>();
-  const [isInComments, setIsInComments] = useState(true);
-  const [isInHistorics, setIsInHistorics] = useState(false);
-  const [taskName, setTaskName] = useState("");
-  const { project, setProject } = useContext(ProjectContext);
-  const { pageId } = useContext(PageContext);
-  const [users, setUsers] = useState<OtherUser[]>([]);
-  const taskNameRef = useRef<any>(null);
-
-  useEffect(() => {
-    const findGroups = async () => {
-      const users = await userService.findAll();
-      setUsers(
-        users.filter(
-          (user) =>
-            user.permissions.find(
-              (permission) => permission.project.id === project?.id
-            ) != undefined
-        )
-      );
-    };
-    findGroups();
-  }, [project]);
-
-  useEffect(() => {
-    setList(undefined);
-    setFilter([]);
-
-    if (taskNameRef.current) {
-      taskNameRef.current.focus();
-      console.log(`FOQUEI PA CARALHO`);
-    }
-  }, [isOpen]);
-
-  useEffect(() => {
-    setTaskName(task?.name ?? "");
-    if (task?.name) {
-      let page = project?.pages.find((page) => pageId == page.id);
-      if (page) {
-        const taskP = page.tasks.find((taskP) => taskP.task.id == task.id);
-        if (taskP) {
-          taskP.task.name = task.name;
-        }
-      }
-
-      setProject!({ ...project! });
-    }
-  }, [task?.name]);
-
-  async function updateNameTask(e: any) {
-    task.name = e.target.value;
-    setTaskName(e.target.value);
-    await taskService.upDate(task, project!.id);
-  }
-
-  const { t } = useTranslation();
 
   return (
     <CenterModal
@@ -88,61 +26,13 @@ export const TaskModal = ({ setIsOpen, isOpen, task, user }: isOpenBro) => {
         setIsOpen(false);
       }}
     >
-      <div className="flex gap-[102px]  w-full h-full ">
-        <div className="flex flex-col gap-12 w-[453px]">
-          <div className="flex flex-col gap-12 w-[453px]">
-            <div className="flex gap-4 items-center">
-              <input
-                className="h3 whitespace-nowrap bg-white dark:bg-modal-grey w-full outline-none"
-                ref={taskNameRef}
-                placeholder={t("withoutname")}
-                value={taskName}
-                onChange={(e) => updateNameTask(e)}
-              ></input>
-            </div>
-            <div className="flex flex-col w-full gap-6">
-              <div className="flex gap-0 w-full">
-                <HeaderCommentAndHistoric
-                  title="comments"
-                  isSelected={isInComments}
-                  setIsInComments={() => {
-                    setIsInComments(!isInComments);
-                    setIsInHistorics(!isInHistorics);
-                  }}
-                />
-                <HeaderCommentAndHistoric
-                  title="historical"
-                  isSelected={isInHistorics}
-                  setIsInComments={() => {
-                    setIsInComments(!isInComments);
-                    setIsInHistorics(!isInHistorics);
-                  }}
-                />
-              </div>
-
-             { isInComments && <CommentsSection task={task} user={user} />}
-            </div>
-          </div>
-        </div>
-        <FilterContext.Provider
-          value={{
-            filterProp: filter,
-            setFilterProp: setFilter,
-            list: list ?? undefined,
-            setList: setList,
-          }}
-        >
-          <div className=" w-[2px] min-h-full bg-[#F2F2F2]"></div>
-          <PropertiesSide
-            filter={filter}
-            setFilter={setFilter}
-            setIsOpen={setIsOpen}
-            setList={setList}
-            task={task}
-            users={users}
-          ></PropertiesSide>
-        </FilterContext.Provider>
-      </div>
+      <TaskModalContent
+        task={task}
+        user={user}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        isInModal={true}
+      />
     </CenterModal>
   );
 };
---
+++ src/components/TaskModal/TaskModalContent.tsx
@@ -0,0 +1,103 @@
+import { FilterContext } from "@/utils/FilterlistContext";
+import { CommentsSection } from "./CommentsAndHistoric/CommentsSection";
+import { HeaderCommentAndHistoric } from "./CommentsAndHistoric/HeaderCommentAndHistoric";
+import { PropertiesSide } from "./PropertiesSide";
+import { ProjectContext } from "@/contexts";
+import { OtherUser, Task, User } from "@/models";
+import { userService, taskService } from "@/services";
+import { FilteredProperty } from "@/types/FilteredProperty";
+import { PageContext } from "@/utils/pageContext";
+import { useState, useContext, useRef, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { TaskName } from "./TaskName";
+
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (boolean: boolean) => void;
+  task: Task;
+  user: User;
+  isInModal?: boolean;
+};
+export const TaskModalContent = ({
+  isOpen,
+  isInModal = false,
+  setIsOpen,
+  task,
+  user,
+}: Props) => {
+  const [filter, setFilter] = useState<FilteredProperty[]>([]);
+  const [list, setList] = useState<FilteredProperty | null>();
+  const [isInComments, setIsInComments] = useState(true);
+  const [isInHistorics, setIsInHistorics] = useState(false);
+  const { project } = useContext(ProjectContext);
+  const [users, setUsers] = useState<OtherUser[]>([]);
+
+  useEffect(() => {
+    setList(undefined);
+    setFilter([]);
+  }, [isOpen]);
+
+  useEffect(() => {
+    const findGroups = async () => {
+      const users = await userService.findAll();
+      setUsers(
+        users.filter(
+          (user) =>
+            user.permissions.find(
+              (permission) => permission.project.id === project?.id
+            ) != undefined
+        )
+      );
+    };
+    findGroups();
+  }, [project]);
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex gap-[102px] w-full h-full ">
+      <div className="flex flex-col gap-12 w-[453px]">
+        { isInModal &&  <TaskName task={task} />}
+          <div className="flex flex-col w-full gap-6">
+            <div className="flex gap-0 w-full">
+              <HeaderCommentAndHistoric
+                title="comments"
+                isSelected={isInComments}
+                setIsInComments={() => {
+                  setIsInComments(!isInComments);
+                  setIsInHistorics(!isInHistorics);
+                }}
+              />
+              <HeaderCommentAndHistoric
+                title="historical"
+                isSelected={isInHistorics}
+                setIsInComments={() => {
+                  setIsInComments(!isInComments);
+                  setIsInHistorics(!isInHistorics);
+                }}
+              />
+            </div>
+            {isInComments && <CommentsSection task={task} user={user} />}
+          </div>
+      </div>
+      <FilterContext.Provider
+        value={{
+          filterProp: filter,
+          setFilterProp: setFilter,
+          list: list ?? undefined,
+          setList: setList,
+        }}
+      >
+        <div className=" w-[2px] min-h-full bg-[#F2F2F2]"></div>
+        <PropertiesSide
+          filter={filter}
+          setFilter={setFilter}
+          setIsOpen={setIsOpen}
+          setList={setList}
+          task={task}
+          users={users}
+        ></PropertiesSide>
+      </FilterContext.Provider>
+    </div>
+  );
+};
