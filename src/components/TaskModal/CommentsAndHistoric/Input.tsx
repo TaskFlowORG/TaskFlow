19b64ec8bdacdda1fab52b4398b03a5cb6c3851a
gabrielluka <gluka.amaral@gmail.com>
2024-04-30 21:39:51 -0300

    chore

--- src/components/TaskModal/CommentsAndHistoric/Input.tsx
+++ src/components/TaskModal/CommentsAndHistoric/Input.tsx
@@ -8,7 +8,7 @@ export const InputComments = (props: Props) => {
   return (
     <input
       type="text"
-      className="lg:text-p14 text-mn w-full flex-1 border-[#d7d7d7] border-[1px] shadow-comment bg-[#f2f2f2] dark:bg-modal-grey flex-1 font-montserrat px-3 py-[10px] rounded-lg"
+      className="lg:text-p14 text-mn w-full  border-[#d7d7d7] border-[1px] shadow-comment bg-[#f2f2f2] dark:bg-modal-grey flex-1 font-montserrat px-3 py-[10px] rounded-lg"
       placeholder={t("write-comment")}
       {...props}
     />
