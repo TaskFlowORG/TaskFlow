2765f980dfccd87872169786449687ac60e7c8b2
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-05-15 19:12:40 -0300

    properties podem ser editadas

--- src/components/OptionsInput/OptionInput.tsx
+++ src/components/OptionsInput/OptionInput.tsx
@@ -13,29 +13,41 @@ type OptionsInputProps = {
   disabled?: boolean;
 };
 
-export const OptionsInput = ({ label, property, disabled }: OptionsInputProps) => {
+export const OptionsInput = ({
+  label,
+  property,
+  disabled,
+}: OptionsInputProps) => {
   const [options, setOptions] = useState<Option[]>(property.options);
 
   useEffect(() => {
     property.options = options;
   }, [options]);
   return (
-    <div className="w-full  h-full">
-      <div className="h-[35%] w-full flex justify-between items-center">
+    <div className="w-full flex max-h-40 h-min flex-col ">
+      <div className="h-min w-full  flex justify-between items-center">
         <p>{label}</p>
         <NeedPermission permission="update">
-        <button
-          className=" h-6 w-6 rotate-45 p-1.5 rounded-full shadow-blur-10 flex justify-center items-center"
-          onClick={() => setOptions([...options, new Option("", "")])}
-        >
-          <IconPlus/>
-        </button>
+          <button
+            className=" h-4 w-4 rotate-45 p-px rounded-full shadow-blur-10 flex justify-center items-center"
+            onClick={() => setOptions([...options, new Option("", "")])}
+          >
+            <IconPlus />
+          </button>
         </NeedPermission>
       </div>
 
-      <div className="w-full h-[55%] overflow-y-scroll none-scrollbar flex-col gap-1 ">
+      <div className="w-full max-h-40 h-full justify-between overflow-y-scroll thin-scrollbar flex-col pr-2 gap-1 ">
         {options.map((options, index) => (
-          <ContentInput disabled={disabled} key={index} index={index} option={options} />
+          <ContentInput
+          remove={(option) => {
+            setOptions(prev => prev.filter((opt) => opt.id !== option.id));
+          }}
+            disabled={disabled}
+            key={index}
+            index={index}
+            option={options}
+          />
         ))}
       </div>
     </div>
