8e22f9baa4f6ae228515cb4fed4a7ee22c8f72b7
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-05-15 14:37:39 -0300

    resolve

--- src/models/user/user/User.ts
+++ src/models/user/user/User.ts
@@ -18,14 +18,9 @@ export class User {
         public mail: string,                  
         public phone: string,                 
         public description: string,                   
-<<<<<<< HEAD
         public points: number,       
-        public authenticate : boolean,                
-=======
-        public points: number,
-        public authenticate: boolean,       
-                
->>>>>>> c2954940089a6368ef1011077a58b7939a8cffb5
+        public authenticate : boolean,                  
+
         public configuration: Configuration,                  
         public permissions: Permission[],       
         public notifications: Notification[],  
