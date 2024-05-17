83d72c34d36bf62e32d71ed16e40897fa87f2215
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-05-17 18:31:48 -0300

    foi

--- src/models/values/DateValued.ts
+++ src/models/values/DateValued.ts
@@ -1,12 +1,17 @@
  
 import { Value } from "./Value";
 
+export class DateWithGoogle {
+    constructor(public dateTime:Date | null,  public idGoogle:string, public id?:number){
+    }
+
+}
 
 export class DateValued extends Value{
 
     value!:Date;
-    constructor(public dateTime:Date | null, public id?:number,){
-        super(dateTime,id);
+    constructor(public date:DateWithGoogle, public id?:number,){
+        super(date,id);
     }
 
 }
