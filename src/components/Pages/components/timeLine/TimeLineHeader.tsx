fc39743f6ff16964f27333a90ef8bece1cd14636
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-05-17 16:03:59 -0300

    timeline concertada

--- src/components/Pages/components/timeLine/TimeLineHeader.tsx
+++ src/components/Pages/components/timeLine/TimeLineHeader.tsx
@@ -24,7 +24,6 @@ export const TimeLineHeader = ({
               className=" flex whitespace-nowrap justify-center py-2 text-p font-alata text-modal-grey dark:text-white
               bg-white dark:bg-modal-grey absolute z-30 border-b-2 h-12 border-primary dark:border-secondary"
               >
-              {specificInterval/interval} - 
               <Interval interval={interval}  specificInterval={specificInterval} isEnd={index == listOfIntervals.length -1}/>
             </div>
             <div className="absolute h-full border-r-2 w-0 border-dashed border-zinc-300 dark:border-zinc-900 z-40" />
