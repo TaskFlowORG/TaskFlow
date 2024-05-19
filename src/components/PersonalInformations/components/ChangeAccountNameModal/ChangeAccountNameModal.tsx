393dc320bf1992ef3b0cb5d862e739b6663408e0
DevMarquardt <gabrielropelatomarquardt@gmail.com>
2024-05-19 20:42:18 -0300

    Falta 3 pequenos detalhes na pagina do chat e 1 detalhe na pagina de configurações pessoais(maioria questão de responsividade), rever amanha com o jonatas e luka

--- src/components/PersonalInformations/components/ChangeAccountNameModal/ChangeAccountNameModal.tsx
+++ src/components/PersonalInformations/components/ChangeAccountNameModal/ChangeAccountNameModal.tsx
@@ -57,12 +57,7 @@ export const ChangeAccountNameModal = ({ close }: ModalUsername) => {
     const onSubmit = async (data: FormData) => {
         if (!verifyUsername(data.username)) {
             setError("username", {
-                message: t("username-incorrect"),
-                type: "manual",
-            });
-        } else if (user?.username === data.newUsernameZod) {
-            setError("newUsernameZod", {
-                message: t("same-username"),
+            message: t("username-not-match"),
                 type: "manual",
             });
         } else {
@@ -97,34 +92,31 @@ export const ChangeAccountNameModal = ({ close }: ModalUsername) => {
 
                 <If condition={step === 0}>
                     <>
-                        <h1 className="text-h3 font-alata text-primary dark:text-secondary">Mudar o nome de usuário</h1>
+                        <h1 className="text-h3 font-alata text-primary dark:text-secondary">{t("change-username")}</h1>
                         <div className="text-dark dark:text-white flex flex-col justify-center gap-4 h-32">
                             <div>
-                                <p className="font-alata text-p text-center">Você tem certeza que deseja alterar seu nome de usuário?</p>
-                            </div>
-                            <div>
-                                <p className="font-alata text-p  text-center text-red-600">Você só pode alterar seu nome de usuário a cada 30 dias!</p>
+                                <p className="font-alata text-p text-center">{t("confirm-username-change")}</p>
                             </div>
                         </div>
                     </>
                 </If>
                 <If condition={step === 1}>
                     <>
-                        <h1 className="text-h3 font-alata text-primary dark:text-secondary">Mudar o nome de usuário</h1>
+                        <h1 className="text-h3 font-alata text-primary dark:text-secondary">{t("change-username")}</h1>
                         <div className="text-dark dark:text-white flex flex-col justify-center h-32">
                             <Input
                                 required
                                 register={{ ...register("username") }}
                                 helperText={errors.username?.message}
                                 classNameInput={"text-p font-montserrat shadow-blur-10 bg-input-grey-opacity border-2 border-input-grey border-opacity-[70%] rounded-md w-96 pl-4 focus:outline-none h-12"}
-                                placeholder={"Nome de usuário atual"}
+                                placeholder={t("current-username")}
                             />
                             <Input
                                 required
                                 register={{ ...register("newUsernameZod") }}
                                 helperText={errors.newUsernameZod?.message}
                                 classNameInput={"text-p font-montserrat shadow-blur-10 bg-input-grey-opacity border-2 border-input-grey border-opacity-[70%] rounded-md w-96 pl-4 focus:outline-none h-12"}
-                                placeholder={"Novo nome de usuário"}
+                                placeholder={t("new-username")}
                             />
                         </div>
                     </>
@@ -140,7 +132,7 @@ export const ChangeAccountNameModal = ({ close }: ModalUsername) => {
                         <Button other="min-w-40 max-w-40" fnButton={() => (handleNextStep())} />
                     </If>
                     <If condition={step === 1}>
-                        <Button other="min-w-40 max-w-40" text="Alterar nome" fnButton={handleSubmit(onSubmit)} />
+                        <Button other="min-w-40 max-w-40" text={t("confirm")} fnButton={handleSubmit(onSubmit)} />
                     </If>
                 </div>
             </div>
--- src/components/icons/ChatIcons/AudioFile.tsx
+++ src/components/icons/ChatIcons/AudioFile.tsx
@@ -2,13 +2,10 @@ export const AudioFile = ({classes}:{classes?:string}) => {
     return (
 <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
 	 viewBox="0 0 32 32"  className={(classes)}>
-<rect x="7" y="4" fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" width="18" height="24"/>
-<polyline fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" points="17,19 17,11 21,12 "/>
-<circle fill="none" stroke="currentColor" stroke-width="2" stroke-miterlimit="10" cx="15" cy="19" r="2"/>
+<rect x="7" y="4" fill="none" stroke="currentColor" stroke-width="2" strokeMiterlimit="10" width="18" height="24"/>
+<polyline fill="none" stroke="currentColor" stroke-width="2" strokeMiterlimit="10" points="17,19 17,11 21,12 "/>
+<circle fill="none" stroke="currentColor" stroke-width="2" strokeMiterlimit="10" cx="15" cy="19" r="2"/>
 </svg>
 
     )
 }
-
-
-
