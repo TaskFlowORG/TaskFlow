17cf1883e9802cb8970e9a5dfc0014491eca853a
gabrielluka <gluka.amaral@gmail.com>
2024-05-23 13:53:52 -0300

    fix:come here bro

--- src/components/Dev/Dev.tsx
+++ src/components/Dev/Dev.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import { Linkedin } from "../linkedin";
+import { Github } from "../Github";
 interface Props {
   name: string;
   description: string;
@@ -8,9 +11,16 @@ export const Dev = ({ name, description }: Props) => {
     // <div className="relative dark:bg-modal-grey min-h-[254px] mt-16 min-w-[315px] mb-8 flex flex-col shadowww rounded-lg gap-4 p-6 pt-12 pb-8 max-w-[318px] w-full">
     <div className="relative dark:bg-modal-grey min-h-[235px] mt-16 justify-between min-w-[315px] mb-8 flex flex-col shadowww rounded-lg gap-4 p-6 pt-8 pb-8 max-w-[318px] w-full">
       <div className="w-full h-5 flex justify-end gap-4">
-        <h3 className="h3 text-primary dark:text-white flex flex-1 items-center">{name}</h3>
-        <span className="bg-slate-400 h-full aspect-square"></span>
-        <span className="bg-slate-400 h-full aspect-square"></span>
+        <h3 className="h3 text-primary dark:text-white flex flex-1 items-center">
+          {name}
+        </h3>
+
+        <span className="bg-transparent h-full text-[#343434] dark:text-white aspect-square relative">
+          <Linkedin />
+        </span>
+        <span className="bg-transparent h-full text-[#343434] dark:text-white aspect-square relative">
+          <Github />
+        </span>
       </div>
       {/* <div className="h-[92px] w-[92px] bg-white dark:bg-modal-grey absolute centeredAbsoluteTop z-[10] rounded-full shadowww">
 
