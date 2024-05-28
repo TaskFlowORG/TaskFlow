bff1c5835fe14059e579cb3a975548b6e2dd5b01
Heloisa Fogaca do Nascimento <heloisafn@weg.net>
2024-05-28 14:01:00 -0300

    a

--- src/app/(all)/(after-login)/[user]/[project]/group/[group]/page.tsx
+++ src/app/(all)/(after-login)/[user]/[project]/group/[group]/page.tsx
@@ -4,7 +4,7 @@ import { Description } from "@/components/Description/Description";
 import { SVGGroupPage } from "@/components/SVGGroupPage/SVGGroupPage";
 import { UsersList } from "@/components/UsersList/UsersList";
 import { useContext, useEffect, useState } from "react"
-import { Group, OtherUser} from "@/models";
+import { Group, OtherUser } from "@/models";
 import { groupService, userService } from "@/services";
 import { ProjectContext } from "@/contexts";
 import { useAsyncThrow } from "@/hooks/useAsyncThrow";
@@ -30,17 +30,17 @@ export default function Home({ params }: { params: { user: string, project: numb
     return (
 
         <div className="group-page w-screen h-screen">
-        <div className="absolute hidden md:flex md:-bottom-36 xl:2xl:bottom-0 -z-50">
+            <div className="absolute hidden md:flex md:-bottom-36 xl:2xl:bottom-0 -z-50">
                 <SVGGroupPage />
             </div>
             <div className="absolute flex h-[75vh] md:hidden top-72">
                 <SVGGroupMobile />
             </div>
-            <div className="w-full h-screen flex flex-col lg:flex-row lg:gap-8 xl:gap-32 pb-16 pt-32">
-            <div className="flex h-full flex-col lg:flex-row w-1/2 lg:ml-10 lg:justify-end">
+            <div className="w-full h-screen md:h-auto flex flex-col lg:flex-row lg:gap-8 xl:gap-32 pb-16 pt-32">
+                <div className="flex h-full flex-col lg:flex-row w-1/2 lg:ml-10 lg:justify-end">
                     <Description project={project} user={user} groupId={params.group} />
                 </div>
-                <div className="flex h-full flex-col lg:flex-row lg:w-1/2 pt-12 lg:pt-0">
+                <div className="flex flex-col lg:flex-row lg:w-1/2 pt-12 lg:pt-0">
                     <UsersList project={project} group={group} user={user} setGroup={setGroup} />
                 </div>
             </div>
