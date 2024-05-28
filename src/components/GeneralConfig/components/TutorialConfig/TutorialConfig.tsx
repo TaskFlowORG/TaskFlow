a7b0a61a16480eec154ce56d7790df26f9050171
Jonatass06 <jonatas_goncalves@estudante.sesisenai.org.br>
2024-05-28 19:07:17 -0300

    tutorial

--- src/components/GeneralConfig/components/TutorialConfig/TutorialConfig.tsx
+++ src/components/GeneralConfig/components/TutorialConfig/TutorialConfig.tsx
@@ -8,49 +8,62 @@ import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export const TutorialConfig = () => {
-    const [modalRefazer, setModalRefazer] = useState<boolean>(false);
-    const { user, setUser } = useContext(UserContext);
-    const router = useRouter();
+  const [modalRefazer, setModalRefazer] = useState<boolean>(false);
+  const { user, setUser } = useContext(UserContext);
+  const router = useRouter();
 
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const refazerTutorial = () => {
-        if (!user || !setUser) return;
-        return( 
-        <CenterModal setCondition={() => setModalRefazer(true)} condition={modalRefazer}><ModalTutorial close={() => setModalRefazer(false)} updateTutorial={() => updateBack()} /></CenterModal>
-       )
-
-
-    }
-
-    const updateBack = async () => {
-        if (!user || !setUser) return;
-        user.configuration.isTutorialMade = false;
-        const updated = await userService.patch(user);
-        setUser({...updated});
-        router.push("/"+user.username);
-    }
-
+  const refazerTutorial = () => {
+    if (!user || !setUser) return;
     return (
-        <>
-            <div className="h-40 w-full flex items-start justify-between pt-6">
-                <div className="w-[65%] flex  flex-col">
-                    <div>
-                        <p className="text-h3 font-alata dark:text-white ">{t("tutorial-config-title")} </p>
-                    </div>
-                    <div>
-                        <p className="text-p font-montserrat">{t("tutorial-config-desc")}</p>
-                    </div>
-                </div>
-                <div className="w-fit h-full flex flex-col justify-around">
-                    <div className="bg-primary dark:bg-secondary w-40 h-11 rounded-md flex items-center justify-center cursor-pointer " onClick={() => setModalRefazer(true)}>
+      <CenterModal
+        setCondition={() => setModalRefazer(true)}
+        condition={modalRefazer}
+      >
+        <ModalTutorial
+          close={() => setModalRefazer(false)}
+          updateTutorial={() => updateBack()}
+        />
+      </CenterModal>
+    );
+  };
 
-                        <p className="text-p font-alata text-contrast ">{t("redo-tutorial-config")}</p>
+  const updateBack = async () => {
+    if (!user || !setUser) return;
+    user.configuration.isTutorialMade = false;
+    const updated = await userService.patch(user);
+    setUser({ ...updated });
+    router.push("/" + user.username);
+  };
 
-                    </div>
-                </div>
-                {modalRefazer ? refazerTutorial() : null}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="h-40 w-full flex items-start justify-between pt-6">
+        <div className="w-[65%] flex  flex-col">
+          <div>
+            <p className="text-h3 font-alata dark:text-white ">
+              {t("tutorial-config-title")}{" "}
+            </p>
+          </div>
+          <div>
+            <p className="text-p font-montserrat">
+              {t("tutorial-config-desc")}
+            </p>
+          </div>
+        </div>
+        <div className="w-fit h-full flex flex-col justify-around">
+          <div
+            className="bg-primary dark:bg-secondary w-40 h-11 rounded-md flex items-center justify-center cursor-pointer "
+            onClick={() => setModalRefazer(true)}
+          >
+            <p className="text-p font-alata text-contrast ">
+              {t("redo-tutorial-config")}
+            </p>
+          </div>
+        </div>
+        {modalRefazer ? refazerTutorial() : null}
+      </div>
+    </>
+  );
+};
